0ca70ed07fad0e43192e209f5d975058097326b9
Karamchakov Igor <125981724+Blizzen1207@users.noreply.github.com>
2023-10-27 21:23:54 +0700

    kod

---
+++ script.js
@@ -0,0 +1,46 @@
+function set_opacity()
+{
+    document.querySelectorAll('.news').forEach(element => 
+    {
+        element.style.opacity = "0.4";
+        element.style.pointerEvents = "none";
+    });
+}
+
+function reset_opacity()
+{
+    document.querySelectorAll('.news').forEach(element => 
+    {
+        element.style.opacity = "1";
+        element.style.pointerEvents = "all";
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() 
+{
+    document.querySelectorAll(".close").forEach(element => 
+    {
+        element.addEventListener("click", function (){
+            document.querySelectorAll(".blocknews-1, .blocknews-2, .blocknews-3").forEach(element => 
+            {
+            element.style.display = "none";
+            });
+            reset_opacity();
+        });
+    });   
+    document.querySelector(".btn-1").addEventListener("click",function ()
+    {
+        document.querySelector(".blocknews-1").style.display = "block";
+        set_opacity();
+    });
+    document.querySelector(".btn-2").addEventListener("click",function ()
+    {
+        document.querySelector(".blocknews-2").style.display = "block";
+        set_opacity();
+    });
+    document.querySelector(".btn-3").addEventListener("click",function ()
+    {
+        document.querySelector(".blocknews-3").style.display = "block";
+        set_opacity();
+    });
+});
